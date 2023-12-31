7812cf6fcb78f0de228cbe0c67125a2b7a51bbdf
nirvanaGarciaV <nirvagarciav@gmail.com>
2023-12-31 17:31:55 -0500

    adding functional swiper

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import '../styles/Catalog.css';
-import '../styles/swiper.css';
+import 'swiper/swiper-bundle.css'; // Importa todos los módulos y estilos necesarios de Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Catalog: React.FC = () => {
   return (
     <div className="container swiper">
       <div className="slide-container">
-
         <div className="swiper-text">
-          <h1> Todas las películas de <span>Studio Ghibli</span> a tu alcance </h1>
-          <h2> Reproduce películas completas de éxito, los Ghibli Originals, historias fantásticas empapádas de sentimiento, buena animación, y mucho más. </h2>
-        </div>
+            <h1> Todas las películas de <span>Studio Ghibli</span> a tu alcance </h1>
+            <h2> Reproduce películas completas de éxito, los Ghibli Originals, historias fantásticas empapádas de sentimiento, buena animación, y mucho más. </h2>
+          </div>
 
-        <div className="card-wrapper swiper-wrapper">
-
-          <div className="card swiper-slide" style={{ backgroundImage: 'url(/assets/movie1.png)' }}>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000 }}
+        >
+          <SwiperSlide className='card' style={{ backgroundImage: 'url(/assets/movie1.png)' }}>
             <div className="movie-details">
               <div className="title-description">
                 <h3 className="title">Howl's Moving Castle</h3>
@@ -22,9 +27,9 @@ const Catalog: React.FC = () => {
                 <p>Jealous of Sophie's closeness to Howl, a wizard, the Witch of Waste transforms her into an old lady. Sophie must find a way to break the spell with the help of Howl's friends, Calcifer and Markl.</p>
               </div>
             </div>
-          </div>
+          </SwiperSlide>
 
-          <div className="card swiper-slide" style={{ backgroundImage: 'url(/assets/movie2.png)' }}>
+          <SwiperSlide className='card' style={{ backgroundImage: 'url(/assets/movie2.png)' }}>
             <div className="movie-details">
               <div className="title-description">
                 <h3 className="title">Kiki's Delivery Service</h3>
@@ -32,9 +37,9 @@ const Catalog: React.FC = () => {
                 <p>Kiki moves to a seaside town with her talking cat, Jiji, to spend a year alone, in accordance with her village's tradition for witches in training. She soon becomes a fixture in the community.</p>
               </div>
             </div>
-          </div>
+          </SwiperSlide>
 
-          <div className="card swiper-slide" style={{ backgroundImage: 'url(/assets/movie3.png)' }}>
+          <SwiperSlide className='card' style={{ backgroundImage: 'url(/assets/movie3.png)' }}>
             <div className="movie-details">
               <div className="title-description">
                 <h3 className="title">My Neighbor Totoro</h3>
@@ -42,42 +47,10 @@ const Catalog: React.FC = () => {
                 <p>Sheeta and her kidnapper are flying to a military prison when their plane is attacked by a gang of air pirates led. She discover the mystical floating city of Laputa, full of treasures.</p>
               </div>
             </div>
-          </div>
+          </SwiperSlide>
 
-          <div className="card swiper-slide" style={{ backgroundImage: 'url(/assets/movie4.png)' }}>
-            <div className="movie-details">
-              <div className="title-description">
-                <h3 className="title">Castle in the Sky</h3>
-                <h4 className="description">Fantasy/Adventure</h4>
-                <p>Jealous of Sophie's closeness to Howl, a wizard, the Witch of Waste transforms her into an old lady. Sophie must find a way to break the spell with the help of Howl's friends, Calcifer and Markl.</p>
-              </div>
-            </div>
-          </div>
+        </Swiper>
 
-          <div className="card swiper-slide" style={{ backgroundImage: 'url(/assets/movie5.png)' }}>
-            <div className="movie-details">
-              <div className="title-description">
-                <h3 className="title">Ponyo</h3>
-                <h4 className="description">Fantasy/Adventure</h4>
-                <p>Sosuke rescues a goldfish trapped in a bottle. The goldfish, who is the daughter of a wizard, transforms herself into a young girl with her father's magic and falls in love with Sosuke.</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="card swiper-slide" style={{ backgroundImage: 'url(/assets/movie6.png)' }}>
-            <div className="movie-details">
-              <div className="title-description">
-                <h3 className="title">The Wind Rises</h3>
-                <h4 className="description">War/Romance</h4>
-                <p>Unable to pursue his dream of becoming a pilot due to his eyesight, Jiro strives hard and draws inspiration from Giovanni Caproni. Soon, he builds two fighter planes that are used in World War II.</p>
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <div className="swiper-button-next swiper-navBtn"></div>
-        <div className="swiper-button-prev swiper-navBtn"></div>
-        <div className="swiper-pagination"></div>
       </div>
     </div>
   );
